5360add45a687166c0005b215f799bd3ba241a75
Hicaro Andre <hicaroandre@hotmail.com>
2025-08-22 18:59:23 -0300

    questions 1 to 3 completed

--- 01question.js
+++ 01question.js
@@ -1,7 +1,7 @@
 
-let numero = 7; 
+let number = 5; 
 
 
 for (let i = 1; i <= 10; i++) {
-  console.log(`${numero} x ${i} = ${numero * i}`);
+  console.log(`${number} x ${i} = ${number * i}`);
 }
---
+++ 02question.js
@@ -0,0 +1,10 @@
+
+function verificarIdade(name, age) {
+  if (age >= 18) {
+    return `${name} tem ${age} anos - Maior de Idade`;
+  } else {
+    return `${name} tem ${age} anos - Menor de Idade`;
+  }
+}
+
+console.log(verificarIdade("Hicaro", 19));
